527ca6223628b9c9afc3f76dd60e46dec5ac8d09
AykhanHasanov <ayxanpro@gmail.com>
2023-10-25 15:45:44 +0200

    I implemented player HP for some reason

--- Assets/Scripts/Player/PlayerStatController.cs
+++ Assets/Scripts/Player/PlayerStatController.cs
@@ -1,22 +1,25 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStatController : MonoBehaviour
 {
     public static PlayerStatController Instance { get; private set; }
-    private Dictionary<Stats, float> stats;
+    private Dictionary<Stats, StatData> stats;
 
     private void Awake()
     {
         Instance = this;
-        stats = new Dictionary<Stats, float>();
-        stats[Stats.Vitality] = 4f;
-        stats[Stats.Strength] = 4f;
-        stats[Stats.Mana] = 3f;
-        stats[Stats.Defense] = 4f;
+        stats = new Dictionary<Stats, StatData>
+        {
+            [Stats.Vitality] = new StatData(4, 4),
+            [Stats.Strength] = new StatData(4, 4),
+            [Stats.Mana] = new StatData(3, 3),
+            [Stats.Defense] = new StatData(4, 4)
+        };
     }
 
-    public float GetStat(Stats statsType)
+    public StatData GetStat(Stats statsType)
     {
         return stats[statsType];
     }
@@ -28,4 +31,21 @@ public enum Stats
     Strength,
     Mana,
     Defense
+}
+
+[Serializable]
+public class StatData
+{
+    public float maxValue;
+    public float currentValue;
+
+    public StatData() : this(1, 1)
+    {
+    }
+
+    public StatData(float maxValue, float currentValue)
+    {
+        this.maxValue = maxValue;
+        this.currentValue = currentValue;
+    }
 }
---
+++ Assets/Scripts/UI managers/StatBarIndicator.cs
@@ -0,0 +1,16 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatBarIndicator : MonoBehaviour
+{
+    [SerializeField] private Image filler;
+    [SerializeField] private TextMeshProUGUI textIndicator;
+    [SerializeField] private bool percentage;
+
+    public void UpdateDisplay(StatData data)
+    {
+        filler.fillAmount = data.currentValue / data.maxValue;
+        textIndicator.text = percentage ? $"{(int)(filler.fillAmount * 100)}%" : data.currentValue.ToString();
+    }
+}
