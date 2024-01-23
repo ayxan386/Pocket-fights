70091c12f13ba04f6c580c08d0e6f7a8ba0fd956
Aykhan <ayxanpro@gmail.com>
2024-01-23 17:12:26 +0100

    two of kind basics

---
+++ Assets/Scripts/Mini games/Two of kind/TwoOfKindManager.cs
@@ -0,0 +1,88 @@
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TwoOfKindManager : MonoBehaviour
+{
+    [SerializeField] private List<PossibleLoot> prizePrefabs;
+    [SerializeField] private TwoOfKindOption optionPrefab;
+    [SerializeField] private Vector2Int optionSize;
+    [SerializeField] private Vector3 offsetFactor;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Transform optionParent;
+    [SerializeField] private CinemachineVirtualCamera miniGameCamera;
+    [SerializeField] private int maxPairChoice = 3;
+
+    private TwoOfKindOption prevOpennedOption;
+    private bool isFirstOption = true;
+
+    private int pairCounter = 0;
+
+    [ContextMenu("Generate options")]
+    public void GenerateOptions()
+    {
+        for (int x = 0; x < optionSize.x; x++)
+        {
+            for (int y = 0; y < optionSize.y; y++)
+            {
+                var option = Instantiate(optionPrefab, optionParent);
+                var pos = option.transform.position;
+                pos.x += (offsetFactor.x + offset.x) * x;
+                pos.z += (offsetFactor.z + offset.z) * y;
+                option.transform.position = pos;
+                option.Manager = this;
+                option.PrizeName = prizePrefabs[Random.Range(0, prizePrefabs.Count)];
+            }
+        }
+
+        StartGame();
+    }
+
+
+    public void OptionOpened(TwoOfKindOption otherOption)
+    {
+        if (isFirstOption)
+        {
+            prevOpennedOption = otherOption;
+            isFirstOption = false;
+            return;
+        }
+
+
+        if (prevOpennedOption == otherOption) return;
+
+        isFirstOption = true;
+        pairCounter++;
+
+        if (prevOpennedOption.PrizeName == otherOption.PrizeName)
+        {
+            prevOpennedOption.enabled = false;
+            otherOption.enabled = false;
+
+            var newDrop = Instantiate(prevOpennedOption.PrizeName.itemPrefab);
+            newDrop.count = 1;
+            EventManager.OnItemAdd?.Invoke(newDrop);
+        }
+        else
+        {
+            prevOpennedOption.Cover();
+            otherOption.Cover();
+        }
+
+        if (pairCounter == maxPairChoice)
+        {
+            EndGame();
+        }
+    }
+
+    private void StartGame()
+    {
+        miniGameCamera.Priority = 55;
+    }
+
+    private void EndGame()
+    {
+        miniGameCamera.Priority = 5;
+    }
+}
