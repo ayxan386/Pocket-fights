2bb16a4ede172f6bf9e6ab85ad4ff305c5107058
ayxan386 <ayxanpro@gmail.com>
2024-02-18 19:43:03 +0100

    Added music

---
+++ Assets/Scripts/Data/SoundList.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+    [CreateAssetMenu(fileName = "SoundList", menuName = "SoundList", order = 0)]
+    public class SoundList : ScriptableObject
+    {
+        [SerializeField] private AudioClip[] clips;
+        private int lastIndex;
+
+        public AudioClip GiveMeRandomClip()
+        {
+            return clips[Random.Range(0, clips.Length)];
+        }
+
+        public AudioClip GiveMeNext()
+        {
+            var res = clips[lastIndex];
+            lastIndex = (lastIndex + 1) % clips.Length;
+
+            return res;
+        }
+    }
+}
---
+++ Assets/Scripts/GameManagers/MusicPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using Data;
+using UnityEngine;
+
+namespace GameManagers
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class MusicPlayer : MonoBehaviour
+    {
+        [SerializeField] private SoundList soundSource;
+
+        private AudioSource audioSource;
+
+        private void Awake()
+        {
+            audioSource = GetComponent<AudioSource>();
+        }
+
+        private IEnumerator Start()
+        {
+            while (true)
+            {
+                audioSource.clip = soundSource.GiveMeNext();
+                audioSource.Play();
+                yield return new WaitUntil(() => !audioSource.isPlaying);
+            }
+        }
+    }
+}
